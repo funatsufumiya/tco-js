be41d6f6e393e9bf62aa7e0c690afbd325d38386
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2015-01-12 03:48:42 +0900

    Update algorithm

--- tco.js
+++ tco.js
@@ -7,56 +7,70 @@
     return m[1];
   }
 
-  function /*String*/ fookRecur(func, funcName){
+  function getArgumentDefs(f){
+    var s = f.toString();
+    var m = s.match(/function[ ]*(?:.*)\((.*)\)[ ]*?{/);
+    return m[1].split(/[ ]*,[ ]*/);
+  }
+
+  function generateAssignExprs(argDefs){
+    var i = 0;
+    var s = '';
+    for (var i=0; i<argDefs.length; i++) {
+      var varName = argDefs[i];
+      s += ('\t\t\t'+varName+' = args['+i+']\n');
+    };
+
+    return s;
+  }
+
+  function makeArray(){
+    return arguments;
+  }
+
+  function /*String*/ fookRecur(func, funcName, argDefs){
     var lines = func.toString().split("\n");
-    var re = new RegExp(funcName+"[ ]*[(]", 'g');
+    var re = new RegExp('(?:return)?[ ]*'+funcName+"[ ]*[(]", 'g');
     var fs = '';
     for(var i=0; i<lines.length; i++) {
       var line = lines[i];
       if(/function[ ]*(.*)\(/.test(line)){
-        fs += (line + "\n");
+        var s = '';
+        s += '\tvar args = null;\n';
+        s += '\twhile(true){\n'
+        s += '\t\tif(args != null){\n';
+        s += generateAssignExprs(argDefs);
+        s += '\t\t}\n'
+        fs += (line + "\n" + s + "\n");
       }else{
-        var s = line.replace(re, "fook_"+funcName+"(");
-        fs += (s + "\n");
+        if(line.indexOf(funcName) > -1){
+          var s = line.replace(re, "args = makeArray(");
+          fs += (s + '; continue;' + "\n");
+        }else{
+          fs += (line + "\n");
+        }
       }
     };
+
+    fs += "}\n";
 
     return fs;
   }
 
   function tco(func, funcName){
     var name = getFuncName(func) || funcName;
+    var argDefs = getArgumentDefs(func);
     if(name == null || name == ''){
       throw Error("tco(): Function name is not specified (please pass the name at 2nd argment)");
     }else{
-      var fookedFuncStr = fookRecur(func, name); /* return as String */
+      var fookedFuncStr = fookRecur(func, name, argDefs); /* return as String */
 
       eval("var mainFunc = "+fookedFuncStr+";");
-
-      function TCO_RETURN(){};
-      var retObj = new TCO_RETURN();
-
-      this["fook_"+name] = function(){
-        retObj['args'] = arguments;
-        return retObj;
-      }
-
-      return function(){
-        var args = arguments;
-
-        while(true){
-          var retVal = mainFunc.apply(null, args);
-          if(retVal instanceof TCO_RETURN){
-            args = retVal['args'];
-            continue;
-          }else{
-            return retVal;
-          }
-        }
-      }
+      return mainFunc;
     }
   }
 
+  G.makeArray = makeArray;
   G.tco = tco;
 
 })();
